80bbd9fcf43ffbd99432eff0c66461dab684daed
Nicolas-1232 <aluno4.hclumiar@gmail.com>
2018-10-27 10:50:30 +0100

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var google:any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapElement:ElementRef;
+  map: any;
+  
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform
+  ) {
+    platform.ready().then(() => {
+      this.initMap();
+    })
+  }
+  initMap(){
+    // HappyCodeLumiarLoc
+    var happycode = {
+      lat: -23.625183,
+      lng: -46.737448
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 16,
+      center: happycode,
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+      position: happycode,
+      map: this.map,
+      title: 'My Position!'
+    });
+  }
+
+}
